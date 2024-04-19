d3cf783520fab815c6ebdef1feb4988022183ac0
victoria-rivaspa13 <162721841+victoria-rivaspa13@users.noreply.github.com>
2024-04-19 11:53:18 -0300

    update maximums.py

--- maximums.py
+++ maximums.py
@@ -1,10 +1,17 @@
-# Replace the "ANSWER HERE" for your answer
-
 def max_of_two(x, y):
-    """Given x and y, that are 2 numbers, return the biggest number."""
-    return "ANSWER HERE" # Remove this line and implement
-
-
+    if x <= y :
+        return y
+    else :
+        return x
+max_of_two(5,4)    
+max_of_two(-2,-3)
+max_of_two(0,0)
 def max_of_three(x, y, z):
-    """Given x, y and z, that are 3 numbers, return the biggest number of the three."""
-    return "ANSWER HERE" # Remove this line and implement
+    if x <= y and z <= y :
+        return y
+    elif y <= z and x <= z :
+        return z
+    else :
+        return x
+max_of_three(-2,-3,-1)
+max_of_three(0,0,0)
